c91481e68ca9e0325d1f2c24b77dd439f06c3dc7
Jesus Flores <jesus.flores1008@yahoo.com>
2026-02-01 08:29:03 -0800

    Initial Commit

---
+++ textai/textaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  textaiApp.swift
+//  textai
+//
+//  Created by Jesus Flores on 2/1/26.
+//
+
+import SwiftUI
+
+@main
+struct textaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ textaiTests/textaiTests.swift
@@ -0,0 +1,17 @@
+//
+//  textaiTests.swift
+//  textaiTests
+//
+//  Created by Jesus Flores on 2/1/26.
+//
+
+import Testing
+@testable import textai
+
+struct textaiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
